93622c54e80aaed9a775d75d9e9d1fb96fff44f3
Renan Freitas <renan.freitas.anjos@gmail.com>
2025-05-18 14:00:20 -0300

    Add styles with tailwindcss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Link, Navigate} from 'react-router-dom';
 import Login from "./components/Login";
 import Perfil from "./components/Perfil";
 import Principal from "./components/Principal";
@@ -12,15 +12,18 @@ export default function App(){
 //<Route path="/principal" element={<Principal/>}/>
     return (
     <>
-    <header>
+    <header className='bg-gradient-to-r from-blue-100 via-white to-blue-100'>
     <Router>
-      <nav>
-        <Link to="/login">Login</Link> | <Link to="/perfil">Perfil</Link> | <Link to="/principal">Principal</Link>
+      <nav className='bg-transparent flex flex-row gap-10 justify-center'>
+        <Link className='bg-blue-200 p-2 text-center rounded hover:bg-grandient-to-r from-blue-400 via-slate-400 to-blue-300' to="/login">Login</Link>
+        <Link className='bg-blue-200 p-2 text-center rounded hover:bg-grandient-to-r from-blue-400 via-slate-400 to-blue-300' to="/perfil">Perfil</Link>
+        <Link className='bg-blue-200 p-2 text-center rounded hover:bg-grandient-to-r from-blue-400 via-slate-400 to-blue-300' to="/principal">Principal</Link>
       </nav>
       <Routes>
+        <Route path='/' element={<Navigate to="/login"/>}/>
         <Route path="/perfil" element={<Perfil />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="*" element={<Pagina404 />}/>
+        <Route  initial path="/login" element={<Login />}/>
+        <Route path="error" element={<Pagina404 />}/>
         <Route path="/principal" element={<Principal/>}/>
       </Routes>
     </Router>
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -38,12 +38,27 @@ export default function Login(){
 
     return(
       <>
-        <form onSubmit = {Verificacao}>
-          <input type="email" onChange={(e)=> setEmail(e.target.value)}/>
-          <input type="password" onChange={(e)=> setSenha(e.target.value)}/>
-    
-          <button type="submit">Logar</button>
-        </form>
+      <div className='flex flex-col justify-center items-center h-screen'>
+        <div className='flex flex-col p-6 rounded-lg shadow-lg w-full max-w-md hover:shadow-slate-700'>
+          <div className=''>
+            <h2 className='text-2xl font-semibold text-center text-blue-700 mb-6'>Login</h2>
+          </div>
+
+          <div className='flex justify-center'>
+            <form className='w-full max-w-sm hover:shadow-slate-400' onSubmit = {Verificacao}>
+              <div className='mb-4'>
+                <input className='w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400' placeholder='login' type="email" onChange={(e)=> setEmail(e.target.value)} required/>
+              </div>
+              <div className='mb-6'>
+                <input className='w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400' placeholder='passowrd' type="password" onChange={(e)=> setSenha(e.target.value)} required/>
+              </div>
+              <div className='mb-4'>
+                <button className='w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition duration-300' type="submit">Logar</button>
+              </div>
+            </form>
+         </div>
+        </div>
+      </div>
       </>
       
     );
